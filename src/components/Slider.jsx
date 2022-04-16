c24c1c9877ed3df6eb9b92a978f194352af04868
Ayush Kumar <ayushsaha111@gmail.com>
2022-04-17 00:10:09 +0530

    second

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useNAvigate } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
 import { db } from "../firebase.config";
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import Spinner from "./Spinner";
-import { async } from "@firebase/util";
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function Slider() {
